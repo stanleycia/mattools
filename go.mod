bc45d3dd7c8aa8dda5d71f17cc8cca512b36b236
Stanley <stanleycia@gmail.com>
2024-10-09 20:19:50 +0700

    first

---
+++ go.mod
@@ -0,0 +1,7 @@
+module mattools
+
+go 1.22
+
+toolchain go1.22.8
+
+require gonum.org/v1/gonum v0.15.1
---
+++ mattools.go
@@ -0,0 +1,22 @@
+package mattools
+
+import (
+    "gonum.org/v1/gonum/mat"
+)
+
+func TopN(m *mat.Dense, l int) {
+    le := l
+    r, c := m.Dims()
+    if le > r {
+        le = r
+    }
+    if le <= 0 {
+        panic("2nd parameter must be positive")
+    }
+    for i := 0; i < le; i++ {
+        for j := 0; j < c; j++ {
+            print(m.At(i, j), "\t")
+        }
+        println()
+    }
+}
